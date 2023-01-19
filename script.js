4a6ce4d16ef0502fd553a12dc5a13248566b37a2
saikumar <saikumarsongali107@gmail.com>
2023-01-19 11:17:55 +0530

    digital clock app

---
+++ script.js
@@ -0,0 +1,63 @@
+function updateTime() {
+    var dateInfo = new Date();
+  
+    /* time */
+    var hr,
+      _min = (dateInfo.getMinutes() < 10) ? "0" + dateInfo.getMinutes() : dateInfo.getMinutes(),
+      sec = (dateInfo.getSeconds() < 10) ? "0" + dateInfo.getSeconds() : dateInfo.getSeconds(),
+      ampm = (dateInfo.getHours() >= 12) ? "PM" : "AM";
+  
+    // replace 0 with 12 at midnight, subtract 12 from hour if 13–23
+    if (dateInfo.getHours() == 0) {
+      hr = 12;
+    } else if (dateInfo.getHours() > 12) {
+      hr = dateInfo.getHours() - 12;
+    } else {
+      hr = dateInfo.getHours();
+    }
+
+  // Current time
+    var currentTime = hr + ":" + _min + ":" + sec;
+
+  // Print time
+  document.getElementsByClassName("hms")[0].innerHTML = currentTime;
+  document.getElementsByClassName("ampm")[0].innerHTML = ampm;
+
+  /* date */
+  var dow = [
+     "Sunday",
+     "Monday",
+     "Tuesday",
+     "Wednesday",
+     "Thursday",
+     "Friday",
+     "Saturday"
+  ],
+   
+ month = [
+     "January",
+     "February",
+     "March",
+     "April",
+     "May",
+     "June",
+     "July",
+     "August",
+     "September",
+     "October",
+     "November",
+     "December"
+ ],
+  day = dateInfo.getDate();
+
+  //store date
+  var currentDate = dow[dateInfo.getDay()] + ", " + month[dateInfo.getMonth()] + " " + day;
+
+  document.getElementsByClassName("date")[0].innerHTML = currentDate;
+};
+
+// print time and date once, then update them every second
+updateTime();
+setInterval(function() {
+  updateTime()
+}, 1000);
